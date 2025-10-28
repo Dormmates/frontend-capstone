9883beeb35dd1fb9605995bde7afcf9ab3aafda4
Giovanni Leo <2232254@slu.edu.ph>
2025-10-28 20:51:28 +0800

    updated customer module

--- src/pages/customer/CustomerViewDepartment.tsx
+++ src/pages/customer/CustomerViewDepartment.tsx
@@ -9,6 +9,8 @@ import type { ShowDataWithSchedules } from "@/types/show";
 import CustomerViewShow from "./CustomerViewShow";
 import { ChevronLeftIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { formatToReadableDate, formatToReadableTime } from "@/utils/date";
+import Countdown from "@/components/Countdown";
 
 const CustomerViewDepartment = () => {
   const { departmentId } = useParams();
@@ -87,6 +89,43 @@ const CustomerViewDepartment = () => {
               <div className="mt-40">
                 <Breadcrumbs backHref="/" items={[{ name: "Shows" }, { name: department?.name ?? "" }]} />
               </div>
+              <div>
+                {departmentShows?.featuredShow && (
+                  <>
+                    <h1 className="mt-10 font-bold text-xl mb-2">Upcoming Show</h1>
+                    <div
+                      onClick={() => {
+                        setSelectedShow(departmentShows.featuredShow);
+                        window.scrollTo({ top: 0, behavior: "smooth" });
+                      }}
+                      key={departmentShows.featuredShow.showId}
+                      className="relative w-full  h-[550px] overflow-hidden group cursor-pointer"
+                    >
+                      <img
+                        className="object-cover w-full h-full transition-transform duration-500 group-hover:scale-105 group-hover:grayscale"
+                        src={departmentShows.featuredShow.showCover}
+                        alt={departmentShows.featuredShow.title}
+                      />
+
+                      <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/70 via-transparent to-transparent">
+                        <div className="w-full p-5  transform translate-y-10 opacity-0 transition-all duration-500 group-hover:translate-y-0 group-hover:opacity-100">
+                          <h1 className="text-2xl text-primary font-bold">{departmentShows.featuredShow.title}</h1>
+                          <p className="text-sm line-clamp-3 text-white">{departmentShows.featuredShow.description}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <h1 className="text-2xl font-bold">
+                      {formatToReadableDate(departmentShows.featuredShow.nextSchedule?.datetime + "")} at{" "}
+                      {formatToReadableTime(departmentShows.featuredShow.nextSchedule?.datetime + "")}
+                    </h1>
+                    <p className="text-xl">
+                      Starts in: <Countdown showDate={departmentShows.featuredShow.nextSchedule?.datetime + ""} />
+                    </p>
+                  </>
+                )}
+              </div>
+              {departmentShows?.featuredShow && <h1 className="mt-10 font-bold text-xl -mb-8">Explore Other Shows</h1>}
               <div className="grid md:grid-cols-2 gap-5 mt-10">
                 {departmentShows?.otherShows.map((show) => (
                   <div
